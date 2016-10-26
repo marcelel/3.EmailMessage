a68e98e593c57b43942ee92aa058fb934d379868
marcelel <marcelel321@gmail.com>
2016-10-26 14:21:49 +0200

    Done

--- src/com/company/EmailMessage.java
+++ src/com/company/EmailMessage.java
@@ -6,6 +6,8 @@ import javax.mail.*;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.io.Console;
+import java.util.function.BooleanSupplier;
 
 /**
  * Created by Marcel on 24.10.2016.
@@ -21,7 +23,13 @@ public class EmailMessage {
     //private LinkedList<String> cc; //optional
     //private LinkedList<String> bcc; // optional
 
-    protected EmailMessage() {}
+    private EmailMessage(Builder _builder) {
+        from = _builder.from;
+        to = new LinkedList<>(_builder.to);
+        subject = _builder.subject;
+        content = _builder.content;
+        mimeType = _builder.mimeType;
+    };
 
     public static Builder builder() { return new EmailMessage.Builder(); }
 
@@ -36,64 +44,88 @@ public class EmailMessage {
         return result;
     }
 
-    public void send(){
-        String host = "localhost";
-        Properties properties = System.getProperties();
-        properties.setProperty("mail.smtp.host", host);
-        Session session = Session.getDefaultInstance(properties);
+    public void send(String _password) {
+        String password = _password;
+        Properties props = System.getProperties();
+        String host = "smtp.gmail.com";
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.user", from);
+        props.put("mail.smtp.password", password);
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
 
-        try{
-            MimeMessage message = new MimeMessage(session);
+        Session session = Session.getDefaultInstance(props);
+        MimeMessage message = new MimeMessage(session);
+
+        try {
             message.setFrom(new InternetAddress(from));
-            for (int i = 0; i < to.size(); i++){
-                message.addRecipient(Message.RecipientType.TO, new InternetAddress(to.get(i)));
+            InternetAddress[] toAddress = new InternetAddress[to.size()];    //jesli to tuaj blad
+
+            // tablica adresow
+            for( int i = 0; i < to.size(); i++ ) {
+                toAddress[i] = new InternetAddress(to.get(i));       //jesli to tutaj blad
             }
+
+            for( int i = 0; i < toAddress.length; i++) {
+                message.addRecipient(Message.RecipientType.TO, toAddress[i]);
+            }
+
             message.setSubject(subject);
             message.setText(content);
-            Transport.send(message);
-            System.out.println("Sent message successfully");
-        }catch (MessagingException e) {
-            e.printStackTrace();
+            Transport transport = session.getTransport("smtp");
+            transport.connect(host, from, password);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+        }
+        catch (AddressException ae) {
+            ae.printStackTrace();
+        }
+        catch (MessagingException me) {
+            me.printStackTrace();
         }
     }
 
-    public static class Builder{
-        EmailMessage result;
+    public static class Builder {
+        private String from; //required (must be e-mail)
+        private LinkedList<String> to; //required at least one (must be e-mail)
+        private String subject; //optional
+        private String content; //optional
+        private String mimeType;  // optional
 
-        public Builder(){
-            result = new EmailMessage();
-        }
+        public Builder() {to = new LinkedList<>();};
 
-        public Builder addFrom (String _from){
-            if(isValidEmailAddress(_from)){ result.from = _from; }
-            else { System.out.println("Invalid email"); }
+        public Builder addFrom(String _from) {
+            from = _from;
             return this;
         }
 
-        public Builder addTo (String _to){
-            if(isValidEmailAddress(_to)){ result.from = _to; }
-            else { System.out.println("Invalid email"); }
+        public Builder addTo(String _to) {
+            to.add(_to);
             return this;
         }
 
-        public Builder addSubject (String _subject){
-            result.subject = _subject;
+        public Builder addSubject(String _subject) {
+            subject = _subject;
             return this;
         }
 
-        public Builder addContent (String _content){
-            result.content = _content;
+        public Builder addContent(String _content) {
+            content = _content;
             return this;
         }
 
-        public Builder addMimeType (String _mimeType){
-            result.mimeType = _mimeType;
+        public Builder addMimeType(String _mimeType) {
+            mimeType = _mimeType;
             return this;
         }
 
-        public EmailMessage build(){
-            return result;
+        public EmailMessage build() {
+            if (isValidEmailAddress(from) && isValidEmailAddress(to.getLast())) {
+                return new EmailMessage(this);
+            } else {
+                return null;
+            }
         }
     }
-
 }
